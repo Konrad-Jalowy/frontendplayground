fc459812d06879377fe60ff7aadeebd919741023
Konrad-Jalowy <sitesystem@wp.pl>
2023-07-26 07:15:36 +0200

    short circuting

---
+++ once.js
@@ -0,0 +1,10 @@
+function once(str, char) {
+    let output = true;
+    output = str.indexOf(char) === -1 ? false : true;
+    output = output && str.indexOf(char) === str.lastIndexOf(char) ? true: false;
+    return output;
+}
+
+console.log(once("hello", "l"));
+console.log(once("helo", "l"));
+console.log(once("hi", "l"));
