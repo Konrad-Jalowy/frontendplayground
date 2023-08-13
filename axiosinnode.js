398110a7f373e0e31f66212eda1ead92ac805bf9
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-13 09:17:25 +0200

    using axios in node

---
+++ axiosinnode.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+axios.get('https://api.chucknorris.io/jokes/random')
+  .then(response => {
+    console.log(response.data.value);
+  })
+  .catch(error => {
+    console.log(error);
+  });
