03bc0835f7e2a475ff8cf671b5a07ff3a8fbb419
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-14 08:04:24 +0200

    comments

--- mixins1.js
+++ mixins1.js
@@ -1,3 +1,7 @@
+//this is blueprint for mixins (kinda like traits in PHP or multiple inheritance on 1 level in Python)
+//your class is class Something extends mixin(mixin1, mixin2, mixin3)
+//great JS-OOP magic
+
 export function mixin(...mixins) {
 
     const fn = function() {};
