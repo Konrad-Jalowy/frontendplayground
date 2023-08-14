1ebf9ce1301dbb51688ddd7fe5bf2423098fede9
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-14 08:02:44 +0200

    comments

--- nodebuffer.js
+++ nodebuffer.js
@@ -1,3 +1,4 @@
+//kinda like Array.from... but you cannot use new Buffer. Only Buffer.from
 var buff = Buffer.from("W tym zdaniu są jakieś polskie znaki.", "utf8");
 //you can pass encoding in toString("utf8")
 console.log(buff.toString());
