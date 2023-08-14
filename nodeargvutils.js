8c44af1e2333c9bd13cd78e23f8bf8f907825b5a
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-14 08:09:24 +0200

    just notes

--- nodeargvutils.js
+++ nodeargvutils.js
@@ -1,7 +1,11 @@
+//thats the blueprint for cmd node app. getting and validating argv
+
+
+//depends on the number of args you want to take
 const args = process.argv.slice(2);
 
 function argv(arg) {
-
+    //args must start with -- in this version
     var index = args.indexOf("--" + arg);
 
     if(index !== -1 && args[index + 1] !== undefined) {
