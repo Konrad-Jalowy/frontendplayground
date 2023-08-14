72427abbb7b463871261b46c40edf96021bc2103
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-14 08:01:56 +0200

    just comments

--- nodebuffer.js
+++ nodebuffer.js
@@ -1,7 +1,7 @@
 var buff = Buffer.from("W tym zdaniu są jakieś polskie znaki.", "utf8");
-
+//you can pass encoding in toString("utf8")
 console.log(buff.toString());
-
+//it overwrites the beginning but not deleting the rest. just like replacing first few indexes in an array
 buff.write("Lorem ipsum");
 
 console.log(buff.toString());
